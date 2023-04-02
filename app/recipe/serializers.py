7666380fead3cf27c2152c223650205f4007e939
Anatoly Rozhkov <anatoly.rozhkov1998@gmail.com>
2023-04-02 11:32:16 +0200

    Finish tag API

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -1,18 +1,68 @@
 """
-Serializers for recipe APIS.
+Serializers for recipe APIs.
 """
 from rest_framework import serializers
 
-from core.models import Recipe
+from core.models import (
+    Recipe,
+    Tag,
+)
+
+
+class TagSerializer(serializers.ModelSerializer):
+    """Serializer for tags."""
+
+    class Meta:
+        model = Tag
+        fields = ['id', 'name']
+        read_only_fields = ['id']
 
 
 class RecipeSerializer(serializers.ModelSerializer):
     """Serializer for recipes."""
+    # many=Ture | it would be a list of items
+    tags = TagSerializer(many=True, required=False)
 
     class Meta:
         model = Recipe
-        fields = ['id', 'title', 'time_minutes', 'price', 'link']
+        fields = ['id', 'title', 'time_minutes', 'price', 'link', 'tags']
         read_only_fields = ['id']
+
+    def _get_or_create_tags(self, tags, recipe):
+        """Handle getting or creating tags as needed."""
+        # get self.user (from view)
+        auth_user = self.context['request'].user
+
+        # create tags separately
+        for tag in tags:
+            tag_object, created = Tag.objects.get_or_create(
+                user=auth_user,
+                **tag  # here we could do name=tag['name'], but in the future there may be more attrs
+            )
+            # add tags to tag list
+            recipe.tags.add(tag_object)
+
+    # overwrite default logic
+    def create(self, validated_data):
+        """Create a recipe."""
+        # remove tags from creating recipe because the model expects
+        # tags to be created separately
+        tags = validated_data.pop('tags', [])
+        recipe = Recipe.objects.create(**validated_data)
+        self._get_or_create_tags(tags, recipe)
+
+        return recipe
+
+    def update(self, instance, validated_data):
+        """Update recipe."""
+        tags = validated_data.pop('tags', None)
+        if tags is not None:
+            instance.tags.clear()
+            self._get_or_create_tags(tags, instance)
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+        instance.save()
+        return instance
 
 
 class RecipeDetailSerializer(RecipeSerializer):
@@ -20,3 +70,4 @@ class RecipeDetailSerializer(RecipeSerializer):
 
     class Meta(RecipeSerializer.Meta):
         fields = RecipeSerializer.Meta.fields + ['description']
+
